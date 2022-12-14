037018d3b97a1dc80382a08b9bafa87d040e1c98
lexzvon <lexzvon@yandex.ru>
2022-12-15 00:11:42 +0300

    review/comments added

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from authentication.models import User
 
 
 class Category(models.Model):
@@ -40,3 +41,22 @@ class GenresTitle(models.Model):
 
     def __str__(self):
         return self.genre
+
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
