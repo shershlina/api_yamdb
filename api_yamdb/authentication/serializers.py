472f5459592cbbd6caf8fbb4ea5b926a77ceebcd
lexzvon <lexzvon@yandex.ru>
2022-12-16 17:55:06 +0300

    validate username

--- api_yamdb/authentication/serializers.py
+++ api_yamdb/authentication/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 
 from .models import User, UserRole
+import re
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = ('username', 'email',)
+
+    def validate_username(self, value):
+        pattern = re.compile(r"^[\w.@+-]+\Z")
+        if pattern.match(value):
+            return value
+        raise serializers.ValidationError(
+                "username должно соответствовать паттерну по документации")
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,4 +22,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'role', 'bio', 'email', 'first_name', 'last_name',)
+        fields = ('username', 'role', 'bio', 'email',
+                  'first_name', 'last_name',)
+    
+    def validate_username(self, value):
+        pattern = re.compile(r"^[\w.@+-]+\Z")
+        if pattern.match(value):
+            return value
+        raise serializers.ValidationError(
+                "username должно соответствовать паттерну по документации")
