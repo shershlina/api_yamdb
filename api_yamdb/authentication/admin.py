165a32d64c11740402f8e3da0239c5ef39a4261f
lina ivanova <shershlina@ya.ru>
2022-12-18 01:15:00 +0700

    small pep8 changes

--- api_yamdb/authentication/admin.py
+++ api_yamdb/authentication/admin.py
@@ -3,7 +3,3 @@ from django.contrib import admin
 from .models import User
 
 admin.site.register(User)
-
-
-
-'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMjM0MjkwLCJqdGkiOiI5ODMwNWJiMDEwM2U0ZmEyYmNkNWZkYWQzZjQyYTgwMSIsInVzZXJfaWQiOjF9.BHuEShTpuzfYYKWMBuotCx3Sc7PKIy90mqMqJyZF7bo'
