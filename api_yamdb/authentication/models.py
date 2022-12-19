be85eec66a658b6a3131cab536ea4d5e43b8c90b
lexzvon <lexzvon@yandex.ru>
2022-12-19 23:23:44 +0300

    change registration and confirmation code

--- api_yamdb/authentication/models.py
+++ api_yamdb/authentication/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from .generate_code import generate_code
-
 
 class UserRole(models.TextChoices):
     """Роли пользователей."""
@@ -15,10 +13,6 @@ class User(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(max_length=254, unique=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
-
-    confirmation_code = models.CharField(max_length=100, null=True,
-                                         verbose_name='Код подтверждения',
-                                         default=generate_code())
     role = models.CharField(
         max_length=10,
         help_text='роль пользователя в системе',
