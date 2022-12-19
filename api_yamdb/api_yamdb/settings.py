9be98465e54ef21a5efd35e7a52a540835321ae6
lexzvon <lexzvon@yandex.ru>
2022-12-20 00:02:17 +0300

    mail changes

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -133,3 +133,5 @@ SIMPLE_JWT = {'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+DOMAIN_NAME = '@test.test'
+DEFAULT_FROM_EMAIL = f'YMDB <test{DOMAIN_NAME}>'
