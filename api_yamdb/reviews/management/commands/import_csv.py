20c1482ae3fcc2c1578ee48a5e548f6237c076f2
Lina Ivanova <shershlina@ya.ru>
2022-12-21 07:53:24 +0700

    csv import corrections

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -1,20 +1,26 @@
 from csv import DictReader
+import os
 from django.core.management import BaseCommand
 
+from api_yamdb.settings import BASE_DIR
+from authentication.models import User
 from reviews.models import Category, Genre, Title, Review, Comment
 
 ALREADY_LOADED_ERROR_MESSAGE = """
-If you need to reload the child data from the CSV file,
+If you need to reload the data from the CSV file,
 first delete the db.sqlite3 file to destroy the database.
 Then, run `python manage.py migrate` for a new empty
 database with tables"""
 
-CSV_DIRS = {
-    Category: 'static/data/category.csv',
-    Genre: 'static/data/genre.csv',
-    Title: 'static/data/title.csv',
-    Review: 'static/data/review.csv',
-    Comment: 'static/data/comment.csv'
+CSV_DIR = os.path.join(BASE_DIR, 'static/data')
+
+CSV_DATA = {
+    'users.csv': User,
+    'category.csv': Category,
+    'genre.csv': Genre,
+    'titles.csv': Title,
+    'review.csv': Review,
+    'comments.csv': Comment
 }
 
 
@@ -22,20 +28,14 @@ class Command(BaseCommand):
     help = "Loads data from category.csv"
 
     def handle(self, *args, **options):
-        for model, path in CSV_DIRS.items():
+        for file, model in CSV_DATA.items():
             if model.objects.exists():
                 print(f'{model.__name__} data already loaded, exiting.')
                 print(ALREADY_LOADED_ERROR_MESSAGE)
-                return
-            print(f'Loading {model.__name__} data')
+                continue
+            print(f'Trying to load {model.__name__} data')
+            path = os.path.join(CSV_DIR, file)
             reader = DictReader(open(path, encoding='utf-8'))
-            fieldnames = reader.fieldnames
-            fields = []
-            for field in fieldnames:
-                fields.append(f'{field}=row["{field}"]')
-            row = ', '.join(fields)
-            table_create = f'{model.__name__}({row})'
-            print(table_create)
-            data = exec(table_create)
-            print('кажется, всё успешно')
-            data.save()
+            for row in reader:
+                model.objects.get_or_create(**row)
+            print(f'{model.__name__} data successfully uploaded')
